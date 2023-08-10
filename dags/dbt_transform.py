a2e4e716bd7e84409510d3ef25c63a23617089a9
raashidsalih <61780547+raashidsalih@users.noreply.github.com>
2023-08-10 09:25:56 +0530

    Update frequency to 6 hours

--- dags/dbt_transform.py
+++ dags/dbt_transform.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 AIRFLOW_HOME = os.getenv('AIRFLOW_HOME')
 cwd = os.path.join(AIRFLOW_HOME, 'dbt_project')
 
-with DAG(dag_id='dbt_transform', schedule_interval="@once", start_date=datetime(2023, 8, 4), catchup=False) as dag:
+with DAG(dag_id='dbt_transform', schedule_interval="0 */6 * * *", start_date=datetime(2023, 8, 4), catchup=False) as dag:
     
     dbt_build = BashOperator(
         task_id = 'dbt_build',
