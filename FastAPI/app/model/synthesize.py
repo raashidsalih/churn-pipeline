f05af281e72d1a0a7c7ccce5bde4681fa042faee
raashidsalih <61780547+raashidsalih@users.noreply.github.com>
2024-09-22 09:56:28 +0530

    update synthesizer

--- FastAPI/app/model/synthesize.py
+++ FastAPI/app/model/synthesize.py
@@ -1,20 +1,18 @@
 # ruff: noqa: F401
 
-import pickle
+import re
 from pathlib import Path
 from sdv.single_table import GaussianCopulaSynthesizer
 
-__version__ = "2.1.0"
-
+filename = "syn_model_3.0.0.pkl"
 BASE_DIR = Path(__file__).resolve(strict=True).parent
-
-# with open(f"{BASE_DIR}/model-{__version__}.pkl", "rb") as f:
-synthesizer = GaussianCopulaSynthesizer.load(filepath=f"{BASE_DIR}/syn_model_2.1.0.pkl")
-
+__version__ = re.search(r'\d+\.\d+\.\d+', filename).group(0)
 
 def synthesize():
+    synthesizer = GaussianCopulaSynthesizer.load(filepath=f"{BASE_DIR}/{filename}")
     return synthesizer.sample(num_rows=1)
 
 
-# if __name__ == "__main__":
-#     df = synthesize()
+if __name__ == "__main__":
+    df = synthesize()
+    print(df)
